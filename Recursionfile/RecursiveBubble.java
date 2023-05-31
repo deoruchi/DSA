67c058d9ba4d7622dec3a90820a70d83a4344351
kurokurotho <deoruchi064@gmail.com>
2023-05-31 14:40:04 +0530

    29th may pending

--- Recursionfile/RecursiveBubble.java
+++ Recursionfile/RecursiveBubble.java
@@ -4,6 +4,27 @@ public class RecursiveBubble {
     // bubble sort using recursion
     public static void main(String[] args) {
         int arr[] = { 1, 3, 5, 2, 5 };
+        bubleSort(arr, arr.length - 1, 0);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+
+    public static int[] bubleSort(int[] a, int s, int e) {
+
+        if (s == 0) {
+            return a;
+        }
+        if (e < s) {
+            if (a[s] < a[e]) {
+                int temp = a[s];
+                a[s] = a[e];
+                a[e] = temp;
+
+            }
+            return bubleSort(a, s, e + 1);
+        } else
+            return bubleSort(a, s - 1, 0);
 
     }
 }
