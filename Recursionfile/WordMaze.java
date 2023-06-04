f110853d7a0ad182d38e951c6e57f4521ffcb977
kurokurotho <deoruchi064@gmail.com>
2023-06-05 00:41:30 +0530

    3rd june

--- Recursionfile/WordMaze.java
+++ Recursionfile/WordMaze.java
@@ -12,24 +12,25 @@ public class WordMaze {
         String p = "";
 
         for (int i = 0; i < board.length; i++) {
+
             for (int j = 0; j < board[0].length; j++) {
 
                 helper(ok, board, p, str, str.length(), i, j);
-                if (p.equals(str))
-                    break;
+
             }
+
         }
 
         // /"",word,maze,board,word.length()
-
+        System.out.println("helloo" + p);
     }
 
     private static void helper(boolean[][] ok, char[][] board, String p, String str, int length, int r, int c) {
         if (length == 0) {
             if (p.equals(str)) {
                 System.out.println(p);
-            } else
-                return;
+            }
+            return;
         }
 
         if (ok[r][c])
@@ -37,19 +38,58 @@ public class WordMaze {
 
         ok[r][c] = true;
 
-        if (length > 0) {
-
-            if (r < board.length)
-                helper(ok, board, p + board[r][c], str, length - 1, r + 1, c);
-            if (c < board[0].length)
-                helper(ok, board, p + board[r][c], str, length - 1, r, c + 1);
-            if (r > 0)
-                helper(ok, board, p + board[r][c], str, length - 1, r - 1, c);
-            if (c > 0)
-                helper(ok, board, p + board[r][c], str, length - 1, r, c - 1);
-        }
+        if (r < board.length - 1)
+            helper(ok, board, p + board[r][c], str, length - 1, r + 1, c);
+        if (c < board[0].length - 1)
+            helper(ok, board, p + board[r][c], str, length - 1, r, c + 1);
+        if (r > 0)
+            helper(ok, board, p + board[r][c], str, length - 1, r - 1, c);
+        if (c > 0)
+            helper(ok, board, p + board[r][c], str, length - 1, r, c - 1);
 
         ok[r][c] = false;
     }
 
 }
+
+// real solution-----------------------------------------
+// class Solution {
+// public boolean exist(char[][] board, String word) {
+// int row = board.length;
+// int col = board[0].length;
+// for (int i=0;i<row;i++){
+// for (int j=0;j<col;j++){
+// if (board[i][j]==word.charAt(0)) {
+// if (faster(board,word,0,i,j,row,col,new boolean[row][col]))return true;
+// }}
+// }
+// return false;
+// }
+// private static boolean faster(char[][] board, String word, int index, int
+// sourceX,
+// int sourceY, int row, int col,boolean[][] tracker){
+// if (index==word.length()-1)return true;
+
+// tracker[sourceX][sourceY]=true;
+// //Left
+// if (sourceY>0 && !tracker[sourceX][sourceY-1] &&
+// board[sourceX][sourceY-1]==word.charAt(index+1)
+// && faster(board,word,index+1,sourceX,sourceY-1,row,col,tracker))return true;
+
+// //Right
+// if (sourceY<col-1 && !tracker[sourceX][sourceY+1] &&
+// board[sourceX][sourceY+1]==word.charAt(index+1)
+// && faster(board,word,index+1,sourceX,sourceY+1,row,col,tracker))return true;
+// //Up
+// if (sourceX>0 && !tracker[sourceX-1][sourceY] &&
+// board[sourceX-1][sourceY]==word.charAt(index+1)
+// && faster(board,word,index+1,sourceX-1,sourceY,row,col,tracker))return true;
+// //Down
+// if (sourceX<row-1 && !tracker[sourceX+1][sourceY] &&
+// board[sourceX+1][sourceY]==word.charAt(index+1)
+// && faster(board,word,index+1,sourceX+1,sourceY,row,col,tracker))return true;
+
+// tracker[sourceX][sourceY]=false;
+// return false;
+// }
+// }
